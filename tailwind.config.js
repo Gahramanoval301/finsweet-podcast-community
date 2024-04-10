19e6461db1fe915088e0868ebb7ad657209c1f7c
Gahramanoval301 <lemangahramanoval@gmail.com>
2024-04-10 17:24:07 +0300

    adding some properties to tailwind.css from the template.extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      container:{
+        center:true,
+      },
+      colors:{
+        primary:'#503AE7',
+        secondary:{
+          basic:'#1AD993',
+          light:'#ABFFE0'
+        },
+        podPurple:'#832BC1',
+        common:{
+          black:'#14142B',
+          grey:'#AFB0B9',
+          light:'#F4F2FF'
+        },
+        white:'#fff',
+      },
+    },
   },
   plugins: [],
 }
