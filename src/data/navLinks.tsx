713508f6499f3cea4f23b9300b50f06c9ec5e1c0
Gahramanoval301 <lemangahramanoval@gmail.com>
2024-04-10 17:25:33 +0300

    creating navLinks array which is contains all routes

---
+++ src/data/navLinks.tsx
@@ -0,0 +1,18 @@
+export const navLinks = [
+    {
+        name:'Podcasts',
+        href:'/podcasts'
+    },{
+        name:'Host',
+        href:'/host'
+    },{
+        name:'About',
+        href:'/about'
+    },{
+        name:'Blog',
+        href:'/blog'
+    },{
+        name:'Contact',
+        href:'/contact'
+    }
+]
