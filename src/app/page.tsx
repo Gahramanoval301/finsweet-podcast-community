a641b274cc8b55df9712b150354e63a9138faeb3
Gahramanoval301 <lemangahramanoval@gmail.com>
2024-04-11 19:04:45 +0300

    adding 4 sectiosn of Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import Brands from "@/components/Home/Brands";
+import HeaderH from "@/components/Home/Header";
+import HowItBegan from "@/components/Home/HowItBegan";
+import WhyIsItFor from "@/components/Home/WhyIsItFor";
+
 export default function Home() {
   return (
-    <main className="">
-      <h1>Salam from page.tsx</h1>
+    <main className="mb-60">
+      <HeaderH />
+      <Brands />
+      <WhyIsItFor />
+      <HowItBegan />
     </main>
   );
 }
