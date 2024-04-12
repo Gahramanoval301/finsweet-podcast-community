6fd16ac90503ae4bccf8e435d43259c7886ad763
Gahramanoval301 <lemangahramanoval@gmail.com>
2024-04-12 18:54:15 +0300

    adding two sections to Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Brands from "@/components/Home/Brands";
 import HeaderH from "@/components/Home/Header";
 import HowItBegan from "@/components/Home/HowItBegan";
+import RecentEpisodes from "@/components/Home/RecentEpisodes";
+import Testimonials from "@/components/Home/Testimonials";
 import WhyIsItFor from "@/components/Home/WhyIsItFor";
 
 export default function Home() {
@@ -10,6 +12,8 @@ export default function Home() {
       <Brands />
       <WhyIsItFor />
       <HowItBegan />
+      <RecentEpisodes />
+      <Testimonials />
     </main>
   );
 }
