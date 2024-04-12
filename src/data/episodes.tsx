a2e87901934f3661ca70d2daf7c40665799385a2
Gahramanoval301 <lemangahramanoval@gmail.com>
2024-04-12 19:00:04 +0300

    creating episodes data

---
+++ src/data/episodes.tsx
@@ -0,0 +1,239 @@
+export const mainEpisodes = [
+    {
+        id: 0,
+        title: 'Ep 1: How to build a world-class business brand',
+        description: 'Lorem ipsum at vero eos et accusam et justo duo dolores et ea rebum.',
+        image:'/Episodes/photo-of-people-sitting-beside-table-3182755.png',
+        subtitle: '',
+        mainContent: {
+            part1st: '',
+            partsnd: ''
+        },
+        subContent: {
+            part1st: '',
+            part2nd: '',
+            properties: ''
+        },
+        audio: '',
+        producer: {
+            name: '',
+            surname: '',
+            job: '',
+            text: '',
+            instagram_link: '',
+            facebook_link: '',
+            linkedIn_link: ''
+        },
+        producing_time: '11/04/2024' //dd/mm/yy
+    }, {
+        id: 1,
+        title: 'Ep 2: Getting the first 100 customers for your business',
+        description: 'Lorem ipsum at vero eos et accusam et justo duo dolores et ea rebum.',
+        image:'/Episodes/man-holding-smartphone.png',
+        subtitle: '',
+        mainContent: {
+            part1st: '',
+            partsnd: ''
+        },
+        subContent: {
+            part1st: '',
+            part2nd: '',
+            properties: ''
+        },
+        audio: '',
+        producer: {
+            name: '',
+            surname: '',
+            job: '',
+            text: '',
+            instagram_link: '',
+            facebook_link: '',
+            linkedIn_link: ''
+        },
+        producing_time: '10/04/2024' //dd/mm/yy
+    }, {
+        id: 2,
+        title: 'Ep 3: Should I raise money for my startup, or not?',
+        description: 'Lorem ipsum at vero eos et accusam et justo duo dolores et ea rebum.',
+        image:'/Episodes/photo-of-women-having-conversation-3194524.png',
+        subtitle: '',
+        mainContent: {
+            part1st: '',
+            partsnd: ''
+        },
+        subContent: {
+            part1st: '',
+            part2nd: '',
+            properties: ''
+        },
+        audio: '',
+        producer: {
+            name: '',
+            surname: '',
+            job: '',
+            text: '',
+            instagram_link: '',
+            facebook_link: '',
+            linkedIn_link: ''
+        },
+        producing_time: '09/04/2024' //dd/mm/yy
+    }, {
+        id: 3,
+        title: '',
+        description: '',
+        image:'',
+        subtitle: '',
+        mainContent: {
+            part1st: '',
+            partsnd: ''
+        },
+        subContent: {
+            part1st: '',
+            part2nd: '',
+            properties: ''
+        },
+        audio: '',
+        producer: {
+            name: '',
+            surname: '',
+            job: '',
+            text: '',
+            instagram_link: '',
+            facebook_link: '',
+            linkedIn_link: ''
+        },
+        producing_time: '05/04/2024' //dd/mm/yy
+    }, {
+        id: 4,
+        title: '',
+        description: '',
+        image:'',
+        subtitle: '',
+        mainContent: {
+            part1st: '',
+            partsnd: ''
+        },
+        subContent: {
+            part1st: '',
+            part2nd: '',
+            properties: ''
+        },
+        audio: '',
+        producer: {
+            name: '',
+            surname: '',
+            job: '',
+            text: '',
+            instagram_link: '',
+            facebook_link: '',
+            linkedIn_link: ''
+        },
+        producing_time: '02/04/2024' //dd/mm/yy
+    }, {
+        id: 5,
+        title: '',
+        description: '',
+        image:'',
+        subtitle: '',
+        mainContent: {
+            part1st: '',
+            partsnd: ''
+        },
+        subContent: {
+            part1st: '',
+            part2nd: '',
+            properties: ''
+        },
+        audio: '',
+        producer: {
+            name: '',
+            surname: '',
+            job: '',
+            text: '',
+            instagram_link: '',
+            facebook_link: '',
+            linkedIn_link: ''
+        },
+        producing_time: '09/03/2024' //dd/mm/yy
+    }, {
+        id: 6,
+        title: '',
+        description: '',
+        image:'',
+        subtitle: '',
+        mainContent: {
+            part1st: '',
+            partsnd: ''
+        },
+        subContent: {
+            part1st: '',
+            part2nd: '',
+            properties: ''
+        },
+        audio: '',
+        producer: {
+            name: '',
+            surname: '',
+            job: '',
+            text: '',
+            instagram_link: '',
+            facebook_link: '',
+            linkedIn_link: ''
+        },
+        producing_time: '08/04/2024' //dd/mm/yy
+    }, {
+        id: 7,
+        title: '',
+        description: '',
+        image:'',
+        subtitle: '',
+        mainContent: {
+            part1st: '',
+            partsnd: ''
+        },
+        subContent: {
+            part1st: '',
+            part2nd: '',
+            properties: ''
+        },
+        audio: '',
+        producer: {
+            name: '',
+            surname: '',
+            job: '',
+            text: '',
+            instagram_link: '',
+            facebook_link: '',
+            linkedIn_link: ''
+        },
+        producing_time: '01/01/2024' //dd/mm/yy
+    }, {
+        id: 8,
+        title: '',
+        description: '',
+        image:'',
+        subtitle: '',
+        mainContent: {
+            part1st: '',
+            partsnd: ''
+        },
+        subContent: {
+            part1st: '',
+            part2nd: '',
+            properties: ''
+        },
+        audio: '',
+        producer: {
+            name: '',
+            surname: '',
+            job: '',
+            text: '',
+            instagram_link: '',
+            facebook_link: '',
+            linkedIn_link: ''
+        },
+        producing_time: '30/03/2024' //dd/mm/yy
+    }
+
+
+] 
