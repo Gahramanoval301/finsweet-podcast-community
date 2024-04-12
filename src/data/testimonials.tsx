d6d5ecfb84088778adbe9a9bbfe599402cd74618
Gahramanoval301 <lemangahramanoval@gmail.com>
2024-04-12 18:59:55 +0300

    creating testimonials data

---
+++ src/data/testimonials.tsx
@@ -0,0 +1,58 @@
+
+export const testimonials = [
+    {
+        id: 0,
+        fullName: 'Eve Madien',
+        image: '',
+        stars: 5,
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem eget condimentum enim libero ultricies amet odio fringilla. Ut nibh morbi augue porta aliquet commodo. Fermentum auctor lacus eget in ut integer viverra sed. Penatibus tortor consequat.'
+    }, {
+        id: 1,
+        fullName: 'Ash Benfred',
+        image: '',
+        stars: 4,
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem eget condimentum enim libero ultricies amet odio fringilla. '
+    }, {
+        id: 2,
+        fullName: 'Ryan Smith',
+        image: '',
+        stars: 5,
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem eget condimentum enim libero ultricies amet odio fringilla. Ut nibh morbi augue porta aliquet commodo. Fermentum auctor lacus eget in ut integer viverra sed. Penatibus tortor consequat.'
+    }, {
+        id: 3,
+        fullName: '',
+        image: '',
+        stars: 4,
+        comment: ''
+    }, {
+        id: 4,
+        fullName: '',
+        image: '',
+        stars: 5,
+        comment: ''
+    }, {
+        id: 5,
+        fullName: '',
+        image: '',
+        stars: 4,
+        comment: ''
+    }, {
+        id: 6,
+        fullName: '',
+        image: '',
+        stars: 1,
+        comment: ''
+    }, {
+        id: 7,
+        fullName: '',
+        image: '',
+        stars: 3,
+        comment: ''
+    }, {
+        id: 8,
+        fullName: '',
+        image: '',
+        stars: 4,
+        comment: ''
+    }
+]
