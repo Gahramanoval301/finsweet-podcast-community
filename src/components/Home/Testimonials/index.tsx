2a45c6af745b3c201be244536ac25e1aadd243b9
Gahramanoval301 <lemangahramanoval@gmail.com>
2024-04-13 19:57:04 +0300

    redesigning of Testimonials section of Home page

--- src/components/Home/Testimonials/index.tsx
+++ src/components/Home/Testimonials/index.tsx
@@ -3,15 +3,15 @@ import TestimonialSwiper from './TestimonialsSwiper'
 
 export default function Testimonials() {
     return (
-        <section className='py-10 xl:px-20 lg:px-10 md:px-8'>
-            <div>
-                <h1>What our listeners say</h1>
-                <p>
+        <section className='py-10 xl:px-20 lg:px-10 md:px-8 relative'>
+            <div className='flex flex-col gap-4'>
+                <h2 className='text-xl font-bold sm:text-2xl lg:font-extrabold lg:text-3xl'>What our listeners say</h2>
+                <p className='w-4/6 md:w-3/6 antialiased text-sm break-words lg:tracking-wide lg:leading-7'>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Mus cras lacus tellus morbi viverra suspendisse ornare.
                     Sit volutpat, volutpat ut netus malesuada enim penatibus non aliquet.
                 </p>
-                <div>
+                <div className='mt-5'>
                     <TestimonialSwiper />
                 </div>
             </div>
