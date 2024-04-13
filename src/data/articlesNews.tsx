6515f325cda15fa9c9abcd362141175853246ffb
Gahramanoval301 <lemangahramanoval@gmail.com>
2024-04-13 21:45:47 +0300

    creating data of 'Articles and News' section of Home page

---
+++ src/data/articlesNews.tsx
@@ -0,0 +1,58 @@
+
+export const articles_news = [
+    {
+        id: 0,
+        title: 'Getting the first 100 customers for your business',
+        image: '/ArticlesNews/bitcoins-and-u-s-dollar-bills-730547.png',
+        description: 'Lorem ipsum at vero eos et accusam et justo duo dolores et ea rebum.',
+        producing_time: '04/11/2024',
+    }, {
+        id: 1,
+        title: 'Apparently we had reached a great height in the atmosphere, ...',
+        image: '/ArticlesNews/top-view-of-assorted-gadgets-on-desk-3568520.png',
+        description: 'Lorem ipsum at vero eos et accusam et justo duo dolores et ea rebum. '
+,        producing_time: '04/10/2024',
+    }, {
+        id: 2,
+        title: '',
+        image: '',
+        description: '',
+        producing_time: '03/20/2024',
+    }, {
+        id: 3,
+        title: '',
+        image: '',
+        description: '',
+        producing_time: '04/02/2024',
+    }, {
+        id: 4,
+        title: '',
+        image: '',
+        description: '',
+        producing_time: '12/30/2023',
+    }, {
+        id: 5,
+        title: '',
+        image: '',
+        description: '',
+        producing_time: '01/04/2024',
+    }, {
+        id: 6,
+        title: '',
+        image: '',
+        description: '',
+        producing_time: '02/09/2024',
+    }, {
+        id: 7,
+        title: '',
+        image: '',
+        description: '',
+        producing_time: '03/10/2024',
+    }, {
+        id: 8,
+        title: '',
+        image: '',
+        description: '',
+        producing_time: '02/11/2024',
+    }
+]
