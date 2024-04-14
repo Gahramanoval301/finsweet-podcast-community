8b054470af25a2c0618452730f0ef3da7d031554
Gahramanoval301 <lemangahramanoval@gmail.com>
2024-04-14 17:12:52 +0300

    substituting  global class names with long design classes

--- src/components/Home/Header/index.tsx
+++ src/components/Home/Header/index.tsx
@@ -4,14 +4,14 @@ export default function HeaderH() {
     return (
         <section className='lg:py-32 sm:py-16 md:px-10 py-10 px-4 lg:px-20 gap-8 md:gap-16 grid grid-cols-1 sm:grid-cols-2 '>
             <div className='flex flex-col gap-6'>
-                <h2 className='text-xl font-bold sm:text-2xl lg:font-extrabold lg:text-3xl'>Become The Hero Of Your Own Story</h2>
-                <p className='antialiased text-sm break-words lg:tracking-wide lg:leading-7'>
+                <h2 className='head-text'>Become The Hero Of Your Own Story</h2>
+                <p className='content'>
                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
                     sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.
                 </p>
                 <div className='bg-primary '>
-                    <input className='antialiased w-7/12 text-sm md:text-base bg-common-light placeholder:text-common-grey py-2 px-3 lg:py-3 lg:px-6 outline-none' type="text" id='emailId' name='emailId' placeholder='Enter Your Email Id' />
-                    <button className='btn text-white antialiased tracking-wide font-bold text-sm w-5/12  py-2 px-3 lg:px-6 lg:py-3'>Subscribe</button>
+                    <input className='input-text ' type="text" id='emailId' name='emailId' placeholder='Enter Your Email Id' />
+                    <button className='btn btn-next-input '>Subscribe</button>
                 </div>
             </div>
             <div className='mx-auto w-4/5'>
