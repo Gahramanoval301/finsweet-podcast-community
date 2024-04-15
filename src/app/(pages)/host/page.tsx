13d013505610c1e996cd6a944732d1c99542706d
Gahramanoval301 <lemangahramanoval@gmail.com>
2024-04-15 22:32:48 +0300

    creating of host page and addingHeader and Brands section to it

---
+++ src/app/(pages)/host/page.tsx
@@ -0,0 +1,11 @@
+import Brands from "@/components/Home/Brands";
+import HeaderHost from "@/components/Host/Header";
+
+export default function Host() {
+  return (
+    <main className="mb-60">
+        <HeaderHost/>
+        <Brands/>
+    </main>
+  );
+}
