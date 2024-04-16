5d0f19d1e231f468ebc7f758ec11f55c7e701dd1
Gahramanoval301 <lemangahramanoval@gmail.com>
2024-04-16 10:40:41 +0300

    creating and designing of CTA section of Host page

---
+++ src/components/Host/CTA/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function CTA() {
+    return (
+        <section className='bg-primary scale-[1.026] lg:px-36 py-20 md:px-16 px-8 my-16 md:mt-36 max-[450px]:px-5 max-[450px]:py-12'>
+            <div className='text-white host-content-box items-start sm:w-3/5'>
+                <h2 className='head-text lg:text-[2.5rem]'>Become The Hero Of Your Own Story</h2>
+                <p className='content'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Mus cras lacus tellus morbi viverra suspendisse ornare.
+                    Sit volutpat, volutpat ut netus malesuada enim penatibus non aliquet.
+                </p>
+                <button className='border-2 btn border-white bg-transparent'>Contact us</button>
+            </div>
+        </section>
+    )
+}
