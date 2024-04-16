0fbd8f5bb2e486f310eee48d322983a0637f8910
Gahramanoval301 <lemangahramanoval@gmail.com>
2024-04-16 10:39:23 +0300

    substituting of long styles with global style class name

--- src/components/Home/Header/index.tsx
+++ src/components/Home/Header/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function HeaderH() {
     return (
-        <section className='lg:py-32 sm:py-16 md:px-10 py-10 px-4 lg:px-20 gap-8 md:gap-16 grid grid-cols-1 sm:grid-cols-2 '>
+        <section className='lg:py-32 sm:py-16 md:px-10 py-10 px-4 lg:px-20 gap-8 md:gap-16 grid-box '>
             <div className='flex flex-col gap-6'>
                 <h2 className='head-text'>Become The Hero Of Your Own Story</h2>
                 <p className='content'>
