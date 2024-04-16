17b07fa5a170987ba405d1f9b08e1db9775d27e6
Gahramanoval301 <lemangahramanoval@gmail.com>
2024-04-16 10:38:51 +0300

    adding sections to Host page

--- src/app/(pages)/host/page.tsx
+++ src/app/(pages)/host/page.tsx
@@ -1,11 +1,21 @@
 import Brands from "@/components/Home/Brands";
+import RecentEpisodes from "@/components/Home/RecentEpisodes";
+import Achievements from "@/components/Host/Achievements";
+import CTA from "@/components/Host/CTA";
+import DreamSection from "@/components/Host/DreamSection";
 import HeaderHost from "@/components/Host/Header";
+import Journey from "@/components/Host/Journet";
 
 export default function Host() {
   return (
-    <main className="mb-60">
-        <HeaderHost/>
-        <Brands/>
+    <main className="mb-64">
+      <HeaderHost />
+      <Brands />
+      <DreamSection />
+      <Journey />
+      <Achievements />
+      <CTA/>
+      <RecentEpisodes />
     </main>
   );
 }
