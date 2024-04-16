615e2f41fc63e871ff5c36a810a6d0586d4c3c18
Gahramanoval301 <lemangahramanoval@gmail.com>
2024-04-16 10:41:17 +0300

    Redesigning of DreamSection section of Host page

--- src/components/Host/DreamSection/index.tsx
+++ src/components/Host/DreamSection/index.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function DreamSection() {
     return (
-        <section className='section-space grid grid-cols-1 sm:grid-cols-2 '>
-            <div className='relative'>
+        <section className='section-space grid-box gap-5 sm:gap-8 '>
+            <div className='host-content-box justify-center'>
                 <h2 className='head-text'>
                     A small story about a big dream  —
                 </h2>
@@ -16,12 +16,12 @@ export default function DreamSection() {
                     Apparently we had reached a great height in the atmosphere,
                     for the sky was a dead black, and
                 </p>
-                <img className='absolute bottom-0 right-0' src="/purpleLine.svg" alt="long green line" />
+                <img className='absolute -top-10 sm:top-0 lg:bottom-20 right-5' src="/purpleLine.svg" alt="long green line" />
 
             </div>
             <div className='relative'>
             <img  src="/Host/man-wearing-a-puffer-jacket-6878718.png" alt="man-wearing-a-puffer-jacket" />
-            <img className='absolute bottom-0 left-0' src="/Host/Wave.svg" alt="wave" />
+            <img className='absolute -bottom-12 -left-14 md:-left-20 lg:-left-16 scale-[0.75] lg:scale-[0.85] xl:scale-100' src="/Host/Wave.svg" alt="wave" />
 
             </div>
         </section>
