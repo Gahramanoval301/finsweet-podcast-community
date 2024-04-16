86789d46b8de84fb8f0f049986b3ac0e75666a07
Gahramanoval301 <lemangahramanoval@gmail.com>
2024-04-16 10:41:28 +0300

    Redesigning of Header section of Host page

--- src/components/Host/Header/index.tsx
+++ src/components/Host/Header/index.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 export default function HeaderHost() {
     return (
-        <section className=' lg:py-32 sm:py-16 md:px-10 py-10 px-4 lg:px-20 gap-8 grid grid-cols-1 sm:grid-cols-2 '>
-            <div className='relative'>
-                <img src="/Host/Image.png" alt="man within microphone logo" />
-                <img className='absolute bottom-10 left-20' src="/greenbLine.svg" alt="small bold green line" />
-                <img className='absolute top-20 left-0' src="/purplebLine.svg" alt="small bold purple line" />
+        <section className=' lg:py-32 sm:py-16 md:px-10 py-10 px-4 lg:px-20 gap-8 grid-box '>
+            <div className='relative flex justify-center sm:justify-end'>
+                <img className='' src="/Host/Image.png" alt="man within microphone logo" />
+                <img className='absolute bottom-10 lg:bottom-[30%] left-5 lg:left-20' src="/greenbLine.svg" alt="small bold green line" />
+                <img className='absolute top-20 left-[10%] lg:left-[25%]' src="/purplebLine.svg" alt="small bold purple line" />
             </div>
-            <div className='relative'>
-                <span className='uppercase text-primary tracking-wide antialiased leading-7'>About the host</span>
+            <div className='host-content-box md:gap-8 justify-start items-start md:py-16 lg:4/5 xl:w-3/5'>
+                <span className='uppercase text-primary tracking-wide antialiased leading-7 hover:text-secondary-basic transition duration-300'>About the host</span>
                 <h2 className='head-text'>
                     Hey there, I’m Andrew Jonson and welcome to my Podcast
                 </h2>
