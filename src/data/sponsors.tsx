16d52b771b9db56e43ceb34591494b51e3e32fe4
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-04-21 19:16:49 +0400

    redownloading images

--- src/data/sponsors.tsx
+++ src/data/sponsors.tsx
@@ -1,7 +1,7 @@
 export const sponsors = [
-    '/footer/Logo1.svg',
-    '/footer/Logo2.svg',
-    '/footer/Logo3.svg',
-    '/footer/Logo4.svg',
-    '/footer/Logo5.svg'
+    '/sponsorsB/sponsor1.svg',
+    '/sponsorsB/sponsor2.svg',
+    '/sponsorsB/sponsor3.svg',
+    '/sponsorsB/sponsor4.svg',
+    '/sponsorsB/sponsor5.svg'
 ]
