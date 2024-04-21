fd1cc5c870edc8a6b8b816340ce1f45c84a3da46
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-04-21 19:28:26 +0400

    the data about teamworkers

---
+++ src/data/ourTeam.tsx
@@ -0,0 +1,40 @@
+export const ourTeam = [
+  {
+    id: 0,
+    fullName: "Jerome Bell",
+    image: "/About/man-with-microphone-pen-in-hand.png",
+    job: "Founder & CEO",
+    linkedin_link: "",
+    instragram_link: "",
+    twitter_link: "",
+  },
+  {
+    id: 1,
+    fullName: "Robert Mill",
+    image: "/About/photo-of-man-wearing-white-shirt-2698918@2x.png",
+    job: "Cofounder",
+    linkedin_link: "",
+    instragram_link: "",
+    twitter_link: "",
+  },
+  {
+    id: 2,
+    fullName: "Rehab Walls",
+    image: "/About/portrait-photo-of-woman-s-face-2530775.png",
+    job: "Producer",
+    linkedin_link: "",
+    instragram_link: "",
+    twitter_link: "",
+  },
+  ,
+  {
+    id: 3,
+    fullName: "Steve Winston",
+    image:
+      "/About/serious-young-man-relaxing-in-smoky-dark-room-with-pipe-led-lamp-4221848.png",
+    job: "Editor",
+    linkedin_link: "",
+    instragram_link: "",
+    twitter_link: "",
+  },
+];
